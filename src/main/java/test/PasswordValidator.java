40e8c8145453a45a2b80a60eba0de97f116cfeeb
Michał Korwel <michal.korwel@ttpsc.pl>
2021-04-13 09:15:36 +0200

    First TDD impl.

--- src/main/java/test/PasswordValidator.java
+++ src/main/java/test/PasswordValidator.java
@@ -1,4 +1,11 @@
 package test;
 
 public class PasswordValidator {
+    public static boolean isValid(String password) {
+        if (password.length() >=5 && password.length() <=10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/test/java/test/TestPassword.java
+++ src/test/java/test/TestPassword.java
@@ -1,4 +1,12 @@
 package test;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestPassword {
+
+    @Test
+    public void TestPasswordLength() {
+        Assert.assertEquals(true, PasswordValidator.isValid("Abc123"));
+    }
 }
